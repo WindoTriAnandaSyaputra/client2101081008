4bc374a07de42bdce530d22f600837ff4e55fef5
Windo Tri Ananda Syaputra <windotriananda@gmail.com>
2022-12-13 13:22:14 +0700

    Initial commit

---
+++ src/main/java/com/windo/client2101081008/controller/AnggotaController.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.windo.client2101081008.controller;
+
+import com.mycompany.client2101081008.FormAnggota;
+import com.windo.client2101081008.service.AnggotaService;
+import com.windo.client2101081008.service.entity.Anggota;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author ASUS
+ */
+public class AnggotaController {
+    private AnggotaService anggotaService;
+    private FormAnggota formAnggota;
+    
+    public AnggotaController(FormAnggota formAnggota){
+        this.formAnggota = formAnggota;
+        anggotaService = new AnggotaService();
+    }
+    
+    public void bersihForm(){
+        formAnggota.getTxtIdAnggota().setText("");
+        formAnggota.getTxtNama().setText("");
+        formAnggota.getTxtAlamat().setText("");
+    }
+    
+    public Anggota saveAnggota(){
+        Anggota anggota = new Anggota();
+        anggota.setNama(formAnggota.getTxtNama().getText());
+        anggota.setAlamat(formAnggota.getTxtAlamat().getText());
+        return anggotaService.saveAnggota(anggota);
+    }
+    
+    public Anggota getAnggotaId(){
+        Long id = Long.parseLong(formAnggota.getTxtIdAnggota().getText());
+        Anggota anggota = anggotaService.getAnggota(id);
+        if(anggota!=null){
+            formAnggota.getTxtNama().setText(anggota.getNama());
+            formAnggota.getTxtAlamat().setText(anggota.getAlamat());
+        }else{
+            JOptionPane.showMessageDialog(formAnggota, "Data tidak ada");
+        }
+        return anggota;
+    }
+}
